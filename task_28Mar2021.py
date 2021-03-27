b3658475643e44b34706dc81e9e5d3c546d2312f
Srikanteswararao Talluri <srikanteswararaotalluri@apples-macbook-pro.local>
2021-03-27 12:10:46 +0530

    new task for today

---
+++ task_28Mar2021.py
@@ -0,0 +1,10 @@
+"""
+task5:
+1. Take two numbers and an arithmetic operator as input from user(ex: num1 num2 arith_sign)
+2. Perform arithmetic operation on num1 and num2
+3. Print the result
+ex: enter first number: 5
+enter second number: 6
+enter arithmetic operator: +
+result : 11
+"""
