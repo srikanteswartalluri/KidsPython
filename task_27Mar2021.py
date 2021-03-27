9823df3798eba0bec80722e6506f254389fc083c
Srikanteswararao Talluri <srikanteswararaotalluri@apples-MacBook-Pro.local>
2021-03-27 11:12:36 +0530

    Initial commit

---
+++ task_27Mar2021.py
@@ -0,0 +1,27 @@
+"""
+Task1:
+1. take your name as input
+2. find the length of your name
+3. print the length
+ex: John
+length is : 4
+"""
+
+"""
+Task2:
+1. take two names as input
+2. Print the longer name
+ex: Name1: Tarun
+Name2: Srikanteswararao
+Srikanteswararao is longer
+"""
+
+"""
+Task3:
+1. take three names as input
+2. Print the longest name
+ex: Name1: Tarun
+Name2: Srikanteswararao
+Name3: Siddu
+Srikanteswararao is the longest
+"""
