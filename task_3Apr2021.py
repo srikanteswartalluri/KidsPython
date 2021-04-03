a2c21a5d27acb32a749c0753fc1bea268e6fb862
srikanteswararao talluri <sritalluri.play@gmail.com>
2021-04-03 11:27:50 +0530

    hint for task

--- task_3Apr2021.py
+++ task_3Apr2021.py
@@ -3,4 +3,9 @@ task7:
 1. Take a number from user
 2. Identify if it is odd or even
 
+hint: 1. If a number is divisible by 2, it is even else it is odd
+2. If a number when divided by 2 leaves a remainder of zero , then that number is divisible by 2  else it is not divisible
+3. Python has '%' operator to find the remainder left when a number is divided
+ex: 11 % 2 => result is 1
+10 % 2 => result is 0
 """
