0d57d85f7a6b91105f557a31925c435d5aa6f734
Talluri, Srikanteswararao <Srikanteswararao.Talluri@netapp.com>
2021-05-09 13:35:50 +0530

    commiting task 11 solution

--- task_9thMay2021.py
+++ task_9thMay2021.py
@@ -4,3 +4,29 @@ task12:
 2. print the squares of the numbers from 1 to n
 """
 
+"""
+task13:
+1. Take a number(n) from user
+2. print as per following condition:
+a. if number is divisible by 4: add 1 to the number and print
+b. if the number is divisilbe by 6: add 2 to the number and print
+c. if the number is divisible by both 4 and 6: subtract 3 from the number
+
+ex:
+if n = 30
+1
+2
+3
+5
+5
+8
+9
+10
+11
+9
+.
+.
+.
+
+"""
+
