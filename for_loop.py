98dcc1f1016748b841be8924286204a4d0309ba3
Talluri, Srikanteswararao <Srikanteswararao.Talluri@netapp.com>
2021-05-09 13:16:45 +0530

    commiting task 11 solution

--- for_loop.py
+++ for_loop.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print(i)
+n = input("Enter a number")
+n = int(n)
+for i in range( n+1):
+    #divisibility by 5
+    if i % 5 == 0 and i % 3 == 0:
+        print("Hello World")
+    elif i % 5 == 0:
+        print("Hello ")
+    elif i % 3 == 0:
+        print("World")
+    else:
+        print(i)
